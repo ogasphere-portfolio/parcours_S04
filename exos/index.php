cf01651f87ccc6fedb1dbe13f4956a251beafdf6
Olivier340 <46643169+Olivier340@users.noreply.github.com>
2023-07-05 07:25:30 +0200

    add readme.html

--- exos/index.php
+++ exos/index.php
@@ -167,12 +167,8 @@ include('../inc/functions.php') ?>
 
 	</main>
 	<footer id="footer">
-		<p>
-			<strong>Besoin d'aide ?</strong> Direction les
-			<a href="https://kourou.oclock.io/ressources/fiches-recap/">
-				<i>📚</i> Fiches recap</a>
-		</p>
-		<p>O'clock</p>
+		
+		<p>Ogasphere</p>
 	</footer>
 	<script src="../js/jquery.min.js"></script>
 	<script src="../js/app.js"></script>
--- inc/functions.php
+++ inc/functions.php
@@ -248,13 +248,9 @@ function displayHtml($data, $result, $content='') {
 	</main>
 
 	<footer id="footer">
-		<p>
-			<strong>Besoin d'aide ?</strong> Direction les
-			<a href="https://kourou.oclock.io/ressources/fiches-recap/">
-				<i>📚</i> Fiches recap
-			</a>
-		</p>
-		<p>O'clock</p>
+		
+		
+		<p>Ogasphere</p>
 	</footer>
 
 	<script src="../js/jquery.min.js"></script>
